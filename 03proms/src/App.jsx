6fe2418f72cb7df71900d6c67c6200af9b2cddd4
Abhay Singh <abhaystudent2002@gmail.com>
2024-05-19 18:02:14 +0530

    commit

---
+++ 03proms/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-400'>
+      tailwind css
+     </h1>
+     <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://media.istockphoto.com/id/959866606/photo/rabbit-4-months-old-sitting-against-white-background.jpg?s=612x612&w=is&k=20&c=FY3K3EtAv7ui7Kdcz1zjy8ZtH8VSUp4CHGDznF_QXTM=" alt="" width="384" height="512" />
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
+    </>
+  )
+}
+
+export default App
---
+++ Vitreact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <h1>hi Bro now i am trying vit React.</h1>
+  )
+}
+
+export default App
